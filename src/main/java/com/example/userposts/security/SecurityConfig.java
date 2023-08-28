a05f4bbda1e419553bf7034aff2e5090130fcfd3
danilkk <ezzfvkoko@gmail.com>
2023-08-28 18:31:16 +0300

    feat: keycloak integration

---
+++ src/main/java/com/example/userposts/security/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.example.userposts.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkUri;
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    private final CorsConfigurationSource corsConfigurationSource;
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors((httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
+                        .configurationSource(corsConfigurationSource)));
+
+        http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/users/**", "/posts/*").permitAll()
+                                .anyRequest()
+                                .authenticated());
+
+        http
+                .oauth2ResourceServer((resourceServerConfigurer) ->
+                        resourceServerConfigurer.jwt((jwtConfigurer) -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter))
+                );
+
+        http
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // return your JWTdecoder
+        // Use NimbusJwtDecoder to decode the JWT
+        OAuth2TokenValidator<Jwt> jwtValidator = JwtValidators.createDefault();
+
+        // Создайте экземпляр NimbusJwtDecoder с указанием URI набора JWK
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkUri).build();
+        jwtDecoder.setJwtValidator(jwtValidator);
+
+        // Добавьте валидатор для проверки наличия ролей в токене
+//        jwtValidator = new DelegatingOAuth2TokenValidator<>(jwtValidator, customJwtValidator);
+//        jwtDecoder.setJwtValidator(jwtValidator);
+
+        return jwtDecoder;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        configuration.addAllowedOrigin(frontendUrl);
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
